926e1c052da7d70f615fdf57dd8b95d96036440d
pavel.kambalin <16853211+kambalin@users.noreply.github.com>
2022-01-11 03:44:26 +0300

    Added github pages demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'strating-test';
+  myRating: number = 0;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StratingModule } from 'nsrs-strating';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StratingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
